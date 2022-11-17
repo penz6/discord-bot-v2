27f7f964a9100eba284e4be06c8607be8d724c3c
penz6 <thetechkid66@gmail.com>
2022-11-16 16:52:56 -0800

    modified bot to be swear filter

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 import discord
 import random
-import token.py
-bot = commands.Bot(command_prefix='>', intents=intents)
-#you have to define bot for some stupid reason so here it is
+import random
+from discord.ext import commands
+from bottoken import bottoken
+from swearfilter import filterwords
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+#you have to define client for some reason
 
 #login message
 @client.event
 async def on_ready():
-    print("We have logged in boys.")
-#select mission
-missionlist = ["Crystalline Caverns","Magma Core","Glacial Strata","Salt Pits","Azure Weald","Sandblasted Corridors","Dense Biozone","Hollow Bough","Fungus Bogs","Chernobyl"]
-randommission = random.choice(missionlist)
-#send the actual mission
-@bot.hybrid_group(fallback="get")
-async def mission(ctx):
-    await ctx.send(randommission)
+    print("Logged into Discord")
+#filter
+@client.event
+async def on_message(message):
+    for banned_word in filterwords:
+        ##loops until word is hit
+        if banned_word in message.content.lower():
+            modchannel = client.get_channel(731715895612670043)
+            ##channel definition
+            await message.author.add_roles(message.guild.get_role(1042529545204539414))
+            await message.delete()
+            ##deletes message
+            logembed = discord.Embed(title="Someone has said something that is in your block list, they have been flagged with the flagged role", color=0x845883)
+            logembed.add_field(name="User?", value=message.author, inline=False)
+            ##embed
+            await modchannel.send(embed=logembed)
+            await message.channel.send("Your message has been deleted due to offensive content, the appropriate people have been contacted.")
+            ##sends stuff
+
+
+
+
 #run bot
-token.run(botoken)
+client.run(bottoken)
 
 
 
