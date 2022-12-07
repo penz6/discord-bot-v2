e5a0d3b8565b46287b7773909aae6647017c821f
penz6 <thetechkid66@gmail.com>
2022-12-07 08:52:00 -0800

    use new libary based off wordlist

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 from bottoken import bottoken
-from profanity_check import predict, predict_prob
+from better_profanity import profanity
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -15,20 +16,21 @@ async def on_ready():
 #filter
 @client.event
 async def on_message(message):
-        if predict_prob([message.content]) > .90:
-            modchannel = client.get_channel(731715895612670043) ##add channel id here
+        profanity.load_censor_words()
+        if profanity.contains_profanity(message.content):
+            modchannel = client.get_channel(731715895612670043)
             ##channel definition
-            await message.author.add_roles(message.guild.get_role(1042529545204539414))## specify role id here
+            await message.author.add_roles(message.guild.get_role(1042529545204539414))
             await message.delete()
             ##deletes message
             logembed = discord.Embed(title="Someone has said something that is in your block list, they have been flagged with the flagged role. They have limited access to the server until you remove it.", color=0x845883)
             logembed.add_field(name="User?", value=message.author, inline=False)
-            logembed.add_field(name="Certainty? (in decimal form)", value=predict_prob([message.content]), inline=False)
             logembed.add_field(name="Content?", value=message.content, inline=False)
             ##embed
             await modchannel.send(embed=logembed)
             await message.channel.send("Your message has been deleted due to offensive content, the appropriate people have been contacted.")
             ##sends stuff
+
             
 
 
